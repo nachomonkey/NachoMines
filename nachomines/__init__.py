8a2b83c13c6a94bc9cba92ecf4e3dee4fc8cd1cf
NachoMonkey <nachomonkey@catbotlabs.com>
2019-04-19 15:05:43 -0600

    Fixed more version stuff

--- nachomines/__init__.py
+++ nachomines/__init__.py
@@ -1,7 +1,4 @@
-try:
-    from scripts.main import __version__
-except ModuleNotFoundError:
-    from nachomines.scripts.main import __version__
 __myname__ = "NachoMines"
 __title__ = "NachoMines"
 __author__ = "NachoMonkey"
+__version__ = "0.1.3"
